e6a4812d0ae7331df89baece4647854330f56437
Mintesinot Necha <165967847+MinteNecha@users.noreply.github.com>
2025-12-19 08:13:23 +0200

    somewhat working test3

---
+++ test3.lua
@@ -0,0 +1,18 @@
+Matrix = {
+    {4, 6, 8},
+    {1, 3, 5},
+    {9, 2, 7}
+}
+
+---[[
+function Task1()
+    Row = {}
+
+    for i = 1, #Matrix do
+        for j = 1, #Matrix[i] do 
+            Row.insert(Matrix)
+        end  
+        print(Row)
+    end
+end
+--]]
